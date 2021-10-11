9f52318cb44d48421e6c174d5c9218fab1a08d58
chandraayu <chandraayuh@gmail.com>
2021-10-11 10:10:16 +0700

    Pertemuan 4

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Chandra Ayu</li>
+        <li>Nama Belakang : Herawaty</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="circle"> 
+        <li> Kp. Ranca Gelang Desa.Pasiripis Kec.Surade Kab.Sukabumi</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="circle">
+        <li>Sukabumi</li>
+    </ul>
+    <h4>Hal yang disukai</h4>
+    <ul type="square">
+        <li>Mendengarkan Musik</li>
+        <li>Menonton Drama korea</li>
+    </ul>
+</section>
