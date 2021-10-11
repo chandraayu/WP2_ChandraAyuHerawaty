96a121b58f05f34e817742e0cafedb9780deb779
chandraayu <chandraayuh@gmail.com>
2021-10-11 09:49:46 +0700

    Pertemuan 2

---
+++ Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+ {
+     echo "<h1>Perkenalkan</h1>";
+     echo"Hallo Nama saya Chandra Ayu Herawaty. saya tinggal di daerah sukabumi, hal yang saya sukai adalah mendengarkan musik kpop.";
+ }
+}
